2730144cb7c7de2b62dc147c0ba140f9ea5bcc92
MAllamandri <maxi_alias17@hotmail.com>
2013-10-23 17:10:44 -0300

    Envio de mensajes acomodado

--- src/AsegurarSA.WebUI/Controllers/LlamadaaController.cs
+++ src/AsegurarSA.WebUI/Controllers/LlamadaaController.cs
@@ -2,26 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Web;
-using System.Web.Mvc;
+using System.Net.Http;
 using System.Net.Mail;
+using System.Web.Http;
+using  System.Web.Http.Routing;
 
 namespace AsegurarSA.WebUI.Controllers
 {
-    public class LlamadaController : Controller
+    public class LlamadaaController : ApiController
     {
-        //
-        // GET: /Llamada/
-
-        public static void Llamada(string NumeroTelefono, string empresa)
+        public void Get()
         {
-            MailMessage message = new MailMessage("asegurarsa.metodos@gmail.com", NumeroTelefono + empresa, "Alarma activada", "Su alarma esta sonando");
+            //"543492521512", "@sms.movistar.net.ar"
+            //@alertas.personal.com.ar    @sms.ctimovil.com.ar
+            MailMessage message = new MailMessage("asegurarsa.metodos@gmail.com", "543404538247@alertas.personal.com.ar", "Alarma activada", "Su alarma esta sonando");
             SmtpClient mySmtpClient = new SmtpClient("smtp.gmail.com");
             NetworkCredential nc = new NetworkCredential("asegurarsa.metodos@gmail.com", "metodosagiles");
             mySmtpClient.Credentials = nc;
             mySmtpClient.EnableSsl = true;
             mySmtpClient.Send(message);
-        }
 
+        }
     }
 }
+
