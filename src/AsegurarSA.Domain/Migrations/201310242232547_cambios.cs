9a63ef3a9ee13a93a5c14ecc1fe2d938a859d673
pibarra <pabloibarr@gmail.com>
2013-10-24 19:37:51 -0300

    Correciones 2 !!!! ahora funciona todo!

---
+++ src/AsegurarSA.Domain/Migrations/201310242232547_cambios.cs
@@ -0,0 +1,18 @@
+namespace AsegurarSA.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class cambios : DbMigration
+    {
+        public override void Up()
+        {
+           
+        }
+        
+        public override void Down()
+        {
+
+        }
+    }
+}
