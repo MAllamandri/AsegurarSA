3bf4caee20debf372437f6b1303c03ea33aa474f
MAllamandri <maxi_alias17@hotmail.com>
2013-11-01 15:40:56 -0300

    migracioens

--- src/AsegurarSA.Domain/Migrations/201311010347228_initial.Designer.cs
+++ src/AsegurarSA.Domain/Migrations/201311010347228_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace AsegurarSA.Domain.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.0.1-21010")]
-    public sealed partial class AlarmaCoordenadas : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(AlarmaCoordenadas));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201310312000042_AlarmaCoordenadas"; }
+            get { return "201311010347228_initial"; }
         }
         
         string IMigrationMetadata.Source
